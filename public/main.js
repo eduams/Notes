3ef5518acfb3fdd0ca2a6bd5cbf72cc88c6c1449
eduams1 <eduardo4ms@gmail.com>
2023-02-01 23:10:25 -0300

    .

--- public/main.js
+++ public/main.js
@@ -1,5 +1,25 @@
 var open;
 
+const router = async () => {
+
+    const routes = [
+        {path: "/1", view: () => console.log("Group1")},
+    ];
+
+}
+
+//vai carregar os notes
+for(var i = 0; i < 9; i++){
+var notesListdiv = document.getElementById('links');
+var list = document.createElement('li');
+var link = document.createElement('a');
+var text = document.createTextNode('Notes group ' + i);
+list.appendChild(link);
+link.appendChild(text);
+notesListdiv.appendChild(list);
+link.href = "#" + i; 
+}
+
 function autoHide(){
     let sidebar = document.getElementById("sidebar");
     var sidebarSize = sidebar.offsetWidth;
