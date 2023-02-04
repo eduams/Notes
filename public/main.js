3f6f71f61c020d323853db9fca3fd2f71e513024
eduams1 <eduardo4ms@gmail.com>
2023-02-04 15:28:32 -0300

    JSON
    Implementação da leitura do conteúdo das notas do arquivo json.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 var open;
+//tamanho do último pacote de notas carregado
+var lenght;
 
 const router = async () => {
 
@@ -8,13 +10,16 @@ const router = async () => {
 
 }
 //vai carregar os notes
-
     notes = fetch("notes.json")
     .then(response => response.json())
     .then(data => showInfo(data))
 
     function showInfo(data){
+        notesObject = {
+                    
+        } 
         for(var i = 0; i < 9; i++){
+
             try {
                 var notesListdiv = document.getElementById('links');
                 var list = document.createElement('li');
@@ -23,6 +28,7 @@ const router = async () => {
                 list.appendChild(link);
                 link.appendChild(text);
                 link.setAttribute('id',i);
+                notesObject[i]= data.groups[i];
                 link.setAttribute('onclick', 'showNotes('+i+')');
                 notesListdiv.appendChild(list);
                 link.href = "#" + i;
@@ -35,13 +41,31 @@ const router = async () => {
     }
 
 function showNotes(i){
+    //eliminar todas as notas carregadas
+    try{
+    var notesClass = document.getElementsByClassName('note');
+    for(var x = 0; x < lenght;x++){
+    notesClass[0].parentNode.removeChild(notesClass[0]);
+    }
+    }
+    catch{}
 
+    //carregar novas notas
+    console.log(notesObject[i]);
+    lenght = Object.keys(notesObject[i]['notes']).length;
     var allNotes = () => {
-        for(var i = 0; i < 9; i++){
+
+        var note = {};
+        var noteText = {};
+        for(var x = 0; x < lenght; x++){
         var content = document.getElementById('content');
-        var note = document.createElement('div');
-        note.setAttribute('class', 'note')
-        content.appendChild(note);
+        note[x] = document.createElement('div');
+        note[x].setAttribute('class', 'note')
+        note[x].setAttribute('id', 'note'+[x])
+        content.appendChild(note[x]);
+        noteText[x] = document.createTextNode(notesObject[i]['notes'][x]['note']);
+        console.log(notesObject[i]['notes'][x])
+        note[x].appendChild(noteText[x])
         }
     };
 
